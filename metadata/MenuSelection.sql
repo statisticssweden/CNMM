cbe650c276ba4106311e98e1dd3ad1051e48220e
MikaelNordberg <mikael.nordberg@scb.se>
2023-04-28 14:44:36 +0200

    Fixed waste category and more

--- metadata/MenuSelection.sql
+++ metadata/MenuSelection.sql
@@ -2,13 +2,13 @@ INSERT INTO [dbo].[MenuSelection]([Menu],[Selection],[PresText],[PresTextS],[Des
 INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'START',N'PO',N'Befolkning',N'Befolkning',N'Befolkningsstatistik',N'Befolkning',N'A',N'script',GETDATE());
 
 INSERT INTO [dbo].[MenuSelection]([Menu],[Selection],[PresText],[PresTextS],[Description],[LevelNo],[SortCode],[Presentation],[MetaId],[UserId],[LogDate]) VALUES(N'START',N'EN',N'Environment',N'Environment',N'Environment statistics',1,N'Environment',N'A',null,N'script',GETDATE());
-INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'START',N'EN',N'Milj�',N'Milj�',N'Milj�statistik',N'Milj�',N'A',N'script',GETDATE());
+INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'START',N'EN',N'Miljö',N'Miljö',N'Miljöstatistik',N'Miljö',N'A',N'script',GETDATE());
 
 INSERT INTO [dbo].[MenuSelection]([Menu],[Selection],[PresText],[PresTextS],[Description],[LevelNo],[SortCode],[Presentation],[MetaId],[UserId],[LogDate]) VALUES(N'PO',N'PO01',N'Population census',N'',N'',3,N'Population',N'A',null,N'script',GETDATE());
 INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'PO',N'PO01',N'Befolkning',N'Census',N'',N'',N'A',N'script',GETDATE());
 
 INSERT INTO [dbo].[MenuSelection]([Menu],[Selection],[PresText],[PresTextS],[Description],[LevelNo],[SortCode],[Presentation],[MetaId],[UserId],[LogDate]) VALUES(N'EN',N'EN01',N'Environment',N'',N'',3,N'Environment',N'A',null,N'script',GETDATE());
-INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'EN',N'EN01',N'Milj�',N'Milj�',N'',N'',N'A',N'script',GETDATE());
+INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'EN',N'EN01',N'Miljö',N'Miljö',N'',N'',N'A',N'script',GETDATE());
 
 INSERT INTO [dbo].[MenuSelection]([Menu],[Selection],[PresText],[PresTextS],[Description],[LevelNo],[SortCode],[Presentation],[MetaId],[UserId],[LogDate]) VALUES(N'PO01',N'CENS_HNMGA',N'',N'',N'',5,N'Population',N'A',null,N'script',GETDATE());
 INSERT INTO [dbo].[MenuSelection_Sve]([Menu],[Selection],[PresText],[PresTextS],[Description],[SortCode],[Presentation],[UserId],[LogDate]) VALUES(N'PO01',N'CENS_HNMGA',null,null,N'',N'',N'A',N'script',GETDATE());
