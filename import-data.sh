43c31141d6e835179615c456f2342d4f6e5ae016
Andreas Nyberg <andreas.nyberg@bra.se>
2025-10-27 15:13:24 +0100

    Adapt to ODBC18 (now in mcr.microsoft.com/mssql/server:2019-latest)

--- import-data.sh
+++ import-data.sh
@@ -2,7 +2,9 @@
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
+    # -N enables encryption (ODBC 18 has Encrypt=Yes by default, but we keep it explicit)
+    # -C trusts the server certificate (only acceptable in dev)
+    /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql -N -C
     if [ $? -eq 0 ]
     then
         echo "setup.sql completed"
@@ -16,4 +18,4 @@ do
 done
 
 #import the data from the csv file
-# /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P $SA_PASSWORD
+# /opt/mssql-tools18/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P $SA_PASSWORD
