03ae52d78c67fd9f6ee9daeb7a1bac31fed81518
Andreas Nyberg <andreas.nyberg@bra.se>
2025-10-30 15:10:34 +0100

    Consistently escape database name [Data]

--- setup.sql
+++ setup.sql
@@ -1,23 +1,20 @@
 CREATE DATABASE Metabase;
 GO
-CREATE DATABASE Data;
+
+CREATE DATABASE [Data];
 GO
 
 USE Metabase
-
 GO
 
 :r cnmm2_3.sql
-
 GO
 
 :r cnmm2_3_metadata.sql
-
 GO
 
 USE [Data]
 GO
 
 :r cnmm2_3_data.sql
-
 GO
